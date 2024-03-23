ce3fa58271e83b6c7b34a814211d9e625b115727
teymoory <mostafa.teymoory.dev@gmail.com>
2024-03-23 23:31:18 +0330

    [creat]: elementary

---
+++ uidKiller.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while [ true ];do
+
+	time=$(date +%r)
+	mem_use=$(free -h | grep  'Mem:' | awk '{print$3}')
+	disk_use=$(df -h | grep 'sda3' | awk '{print$3}')
+	echo "Time : $time , mem use : $mem_use , disk use : $disk_use"
+	mem_use=$(echo $mem_use | tr -d [:alpha:])
+	mem_use=$(echo "$mem_use * 10" | bc | tr -d [:punct:])
+	
+	if [ $mem_use -ge 170 ];then
+		killing=$(ps -e | grep 'firefox' | awk '{print$1}')
+		memU=$(free | grep  'Mem:' | awk '{print$3}')
+		echo "break => mem use : $memU" >> mt.txt
+		kill % $killing
+	fi
+
+	sleep 1
+
+done
+
+
